3230dcb360bcead5cb4da77f94880cc9016b011b
ronjdaugherty29@gmail.com <ronjdaugherty29@gmail.com>
2019-05-05 08:49:20 -0400

    changed tab name and removed fab

--- app/src/main/java/com/rondaugherty/weatherappcodechallenge/MainActivity.kt
+++ app/src/main/java/com/rondaugherty/weatherappcodechallenge/MainActivity.kt
@@ -3,8 +3,6 @@ package com.rondaugherty.weatherappcodechallenge
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import com.rondaugherty.weatherappcodechallenge.ui.main.SectionsPagerAdapter
 
@@ -18,11 +16,8 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
+
     }
 }
