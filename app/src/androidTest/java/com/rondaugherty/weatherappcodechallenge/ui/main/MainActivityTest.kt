7a74426d679f0787556d98b6ec4f2bc4a73452af
ronjdaugherty29@gmail.com <ronjdaugherty29@gmail.com>
2019-05-06 19:01:29 -0400

    added some Ui testing

---
+++ app/src/androidTest/java/com/rondaugherty/weatherappcodechallenge/ui/main/MainActivityTest.kt
@@ -0,0 +1,255 @@
+package com.rondaugherty.weatherappcodechallenge.ui.main
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
+import androidx.test.runner.AndroidJUnit4
+import com.rondaugherty.weatherappcodechallenge.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Rule
+    @JvmField
+    var mGrantPermissionRule =
+        GrantPermissionRule.grant(
+            "android.permission.ACCESS_COARSE_LOCATION"
+        )
+
+    @Test
+    fun mainActivityTest() {
+
+
+        val textView2 = onView(
+            allOf(
+                withText("CURRENT TEMP"),
+                childAtPosition(
+                    allOf(
+                        withContentDescription("Current Temp"),
+                        childAtPosition(
+                            IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("CURRENT TEMP")))
+
+        val textView3 = onView(
+            allOf(
+                withText("CURRENT TEMP"),
+                childAtPosition(
+                    allOf(
+                        withContentDescription("Current Temp"),
+                        childAtPosition(
+                            IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textView3.check(matches(withText("CURRENT TEMP")))
+
+
+
+
+
+
+        val textView4 = onView(
+            allOf(
+                withText("5 DAY"),
+                childAtPosition(
+                    allOf(
+                        withContentDescription("5 Day"),
+                        childAtPosition(
+                            IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
+                            1
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textView4.check(matches(withText("5 DAY")))
+
+        val textView5 = onView(
+            allOf(
+                withId(R.id.dateTimeTextView), withText("Mon, May 6"),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textView5.check(matches(withText("Mon, May 6")))
+
+        val textView6 = onView(
+            allOf(
+                withId(R.id.forecastTemp), withText("62°"),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        textView6.check(matches(withText("62°")))
+
+        val textView7 = onView(
+            allOf(
+                withId(R.id.forecastTemp), withText("62°"),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        textView7.check(matches(withText("62°")))
+
+        val textView8 = onView(
+            allOf(
+                withId(R.id.forecastTemp), withText("62°"),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        textView8.check(matches(withText("62°")))
+
+        val imageView3 = onView(
+            allOf(
+                withId(R.id.weatherIconImageView), withContentDescription("TODO"),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        imageView3.check(matches(isDisplayed()))
+
+        val imageView4 = onView(
+            allOf(
+                withId(R.id.weatherIconImageView), withContentDescription("TODO"),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        imageView4.check(matches(isDisplayed()))
+
+        val tabView = onView(
+            allOf(
+                withContentDescription("5 Day"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.tabs),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        tabView.perform(click())
+
+        val tabView2 = onView(
+            allOf(
+                withContentDescription("5 Day"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.tabs),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        tabView2.perform(click())
+
+        val tabView3 = onView(
+            allOf(
+                withContentDescription("Current Temp"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.tabs),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        tabView3.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
